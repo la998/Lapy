fb5c837fce232d3c8b76eb3a4ab166c7a9facbb9
DanceHill <Mrluchanglong@163.com>
2017-12-21 14:19:42 +0800

    add new models and back front register

--- WebModel/admin.py
+++ WebModel/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from WebModel.models import Blog, Contact, Tag
+
 
 # Register your models here.
+
+
+class ContactAdmin(admin.ModelAdmin):
+    fields = ('name', 'email')
+
+
+admin.site.register(Contact, ContactAdmin)
+admin.site.register([Blog, Tag])
+
+
--- WebModel/models.py
+++ WebModel/models.py
@@ -1,10 +1,28 @@
 from django.db import models
-from time import time
+import datetime
+from django.utils.timezone import utc
 
 # Create your models here.
 
 
 class Blog(models.Model):
     title = models.CharField(max_length=50)
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    age = models.IntegerField(default=0, null=True)
+    email = models.EmailField(null=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, null=True)
+
+    def __unicode__(self):
+        return self.name
 
 
